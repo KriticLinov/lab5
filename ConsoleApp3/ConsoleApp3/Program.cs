10350184c78ee80c81525fe11fe48b8267f6b294
Nikita Pomelov <krt3298@gmail.com>
2024-02-01 10:08:30 +0300

    .

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = 1;
+            int s = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                p *= i;
+                s += p;
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
